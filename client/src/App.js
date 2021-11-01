f89f67cbb54c216e0443e9e93bdbdfc414bb2125
louis lising <louis@louiss-MBP.hub>
2021-11-01 15:59:16 +1300

    added docker

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Nav from "./components/Nav";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import UsersTable from "./components/UsersTable";
+import PhoneNumberTable from "./components/PhoneNumberTable";
+import EmailTable from "./components/EmailTable";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           Hello welcome to my first python django react Full stack applications
           <Switch>
             <Route exact path="/" component={UsersTable} />
+            <Route path="/phone" component={PhoneNumberTable} />
+            <Route path="/email" component={EmailTable} />
           </Switch>
         </h1>
       </div>
---
+++ client/src/components/PhoneNumberTable.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PhoneNumberTable() {
+  return <h1>This is the Phone number table</h1>;
+}
+
+export default PhoneNumberTable;
